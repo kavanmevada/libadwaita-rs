5f8fe3d59def0326fe8ffd17b0f74b406187768b
Sophie Herold <sophie@hemio.de>
2022-01-29 16:53:56 +0100

    doc: better example

--- libadwaita/src/lib.rs
+++ libadwaita/src/lib.rs
@@ -33,23 +33,18 @@
 //! # use libadwaita as adw;
 //! use adw::prelude::*;
 //!
-//! use adw::{ActionRow, ApplicationWindow, HeaderBar};
-//! use gtk::{Application, Box, ListBox, Orientation};
+//! use adw::{ActionRow, Application, ApplicationWindow, HeaderBar};
+//! use gtk::{Box, ListBox, Orientation, SelectionMode};
 //!
 //! fn main() {
 //!     let application = Application::builder()
 //!         .application_id("com.example.FirstAdwaitaApp")
 //!         .build();
 //!
-//!     application.connect_startup(|_| {
-//!         adw::init();
-//!     });
-//!
 //!     application.connect_activate(|app| {
 //!         // ActionRows are only available in Adwaita
 //!         let row = ActionRow::builder()
 //!             .activatable(true)
-//!             .selectable(false)
 //!             .title("Click me")
 //!             .build();
 //!         row.connect_activated(|_| {
@@ -61,23 +56,21 @@
 //!             .margin_end(32)
 //!             .margin_bottom(32)
 //!             .margin_start(32)
-//!             // the content class makes the list look nicer
-//!             .css_classes(vec![String::from("content")])
+//!             .selection_mode(SelectionMode::None)
+//!             // makes the list look nicer
+//!             .css_classes(vec![String::from("boxed-list")])
 //!             .build();
 //!         list.append(&row);
 //!
 //!         // Combine the content in a box
 //!         let content = Box::new(Orientation::Vertical, 0);
 //!         // Adwaitas' ApplicationWindow does not include a HeaderBar
-//!         content.append(
-//!             &HeaderBar::builder()
-//!                 .title_widget(&adw::WindowTitle::new("First App", ""))
-//!                 .build(),
-//!         );
+//!         content.append(&HeaderBar::new());
 //!         content.append(&list);
 //!
 //!         let window = ApplicationWindow::builder()
 //!             .application(app)
+//!             .title("First App")
 //!             .default_width(350)
 //!             // add content to window
 //!             .content(&content)
