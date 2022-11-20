d84afa93c950d9253c9f1329dd9a4039ed205311
Bilal Elmoussaoui <belmouss@redhat.com>
2022-11-20 13:12:03 +0100

    subclass: Allow subclassing MessageDialog

--- libadwaita/src/subclass/mod.rs
+++ libadwaita/src/subclass/mod.rs
@@ -7,6 +7,9 @@ pub mod combo_row;
 #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_2")))]
 pub mod entry_row;
 pub mod expander_row;
+#[cfg(any(feature = "v1_2", feature = "dox"))]
+#[cfg_attr(feature = "dox", doc(cfg(feature = "v1_2")))]
+mod message_dialog;
 pub mod preferences_group;
 pub mod preferences_page;
 pub mod preferences_row;
@@ -24,6 +27,9 @@ pub mod prelude {
     #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_2")))]
     pub use super::entry_row::EntryRowImpl;
     pub use super::expander_row::ExpanderRowImpl;
+    #[cfg(any(feature = "v1_2", feature = "dox"))]
+    #[cfg_attr(feature = "dox", doc(cfg(feature = "v1_2")))]
+    pub use super::message_dialog::{MessageDialogImpl, MessageDialogImplExt};
     pub use super::preferences_group::PreferencesGroupImpl;
     pub use super::preferences_page::PreferencesPageImpl;
     pub use super::preferences_row::PreferencesRowImpl;
